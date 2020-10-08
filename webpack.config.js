ba82ebc542582a54ce9afd7943b387d9ccb828cc
Anton <dubovik02@yandex.ru>
2020-10-08 22:22:02 +0300

    Before adaptive

---
+++ webpack.config.js
@@ -0,0 +1,69 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackMd5Hash = require('webpack-md5-hash');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const webpack = require('webpack');
+const isDev = process.env.NODE_ENV === 'development';
+
+module.exports = {
+    entry: { main: './src/index.js' },
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: '[name].[chunkhash].js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+              test: /\.(gif|png|jpe?g|jpg|svg|ico)$/i,
+              use: [
+                'file-loader?name=./images/[name].[ext]',
+                {
+                  loader: 'image-webpack-loader',
+                  options: {}
+                },
+              ],
+            },
+            {
+                test: /\.css$/,
+                use:  [(isDev ? 'style-loader': MiniCssExtractPlugin.loader), 'css-loader', 'postcss-loader']
+            },
+            {
+              test: /\.(eot|ttf|woff|woff2)$/,
+              loader: 'file-loader?name=./vendor/[name].[ext]'
+            }
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({filename: 'style.[contenthash].css'}),
+        new OptimizeCssAssetsPlugin({
+          assetNameRegExp: /\.css$/g,
+          cssProcessor: require('cssnano'),
+          cssProcessorPluginOptions: {
+                  preset: ['default'],
+          },
+          canPrint: true
+        }),
+        new HtmlWebpackPlugin({
+            inject: false,
+            template: './src/index.html',
+            filename: 'index.html'
+        }),
+        new HtmlWebpackPlugin({
+          inject: false,
+          template: './src/saved.html',
+          filename: 'saved.html'
+        }),
+        new WebpackMd5Hash(),
+        new webpack.DefinePlugin({
+          'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+        })
+    ]
+};
